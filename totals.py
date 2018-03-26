7bff89f9298f1bd4e6befd2d81569f6596c5d209
C.M.A. Assen <s2582562@lett-edu-145-138.let.rug.nl>
2018-03-26 10:36:48 +0200

    added totals.py

---
+++ totals.py
@@ -0,0 +1,11 @@
+def main():
+    total_north = sum([155477, 259821, 267156, 255287, 275202])
+    total_south = sum([149750, 246428, 268142, 259098, 242190])
+    patat_north = sum([23, 29, 42, 22, 21])
+    friet_north = sum([12, 12, 24, 12, 17])
+    patat_south = sum([10, 3, 35, 5, 10])
+    friet_south = sum([19, 27, 59, 40, 34])
+    
+    print("total_north: {0}\ntotal_south: {1}\npatat_north: {2}\nfriet_north: {3}\npatat_south: {4}\nfriet_south: {5}".format(total_north, total_south, patat_north, friet_north, patat_south, friet_south))
+
+main()
